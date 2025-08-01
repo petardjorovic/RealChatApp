7d70ef5a9fc8fdb89001ea4624ef2d27f9fea1e9
petardjorovic <perodjorovic@gmail.com>
2025-08-01 21:19:12 +0200

    finish refresh token route

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,11 +1,20 @@
-import z from "zod";
 import catchErrors from "../utils/catchErrors.js";
-import { createAccount, loginUser } from "../services/auth.service.js";
-import { CREATED, OK } from "../constants/http.js";
-import { clearAuthCookies, setAuthCookies } from "../utils/cookies.js";
+import {
+  createAccount,
+  loginUser,
+  refreshUserAccessToken,
+} from "../services/auth.service.js";
+import { CREATED, OK, UNAUTHORIZED } from "../constants/http.js";
+import {
+  clearAuthCookies,
+  getAccessTokenCookieOptions,
+  getRefreshTokenCookieOptions,
+  setAuthCookies,
+} from "../utils/cookies.js";
 import { loginSchema, registerSchema } from "./auth.schemas.js";
 import { verifyToken } from "../utils/jwt.js";
 import SessionModel from "../models/session.model.js";
+import appAssert from "../utils/appAssert.js";
 
 export const registerHandler = catchErrors(async (req, res) => {
   // validate request
@@ -40,8 +49,8 @@ export const loginHandler = catchErrors(async (req, res) => {
 });
 
 export const logoutHandler = catchErrors(async (req, res) => {
-  const accessToken = req.cookies.accessToken;
-  const { payload } = verifyToken(accessToken); // moze i ovde kroz generic da se postavi AccessTokenPaylod ili RefeshTokenPayload
+  const accessToken = req.cookies.accessToken as string | undefined;
+  const { payload } = verifyToken(accessToken || ""); // moze i ovde kroz generic da se postavi AccessTokenPaylod ili RefeshTokenPayload
 
   if (payload) {
     await SessionModel.findByIdAndDelete(payload.sessionId);
@@ -50,4 +59,21 @@ export const logoutHandler = catchErrors(async (req, res) => {
   return clearAuthCookies(res).status(OK).json({
     message: "Logout successfull",
   });
+});
+
+export const refreshHandler = catchErrors(async (req, res) => {
+  const refreshToken = req.cookies.refreshToken as string | undefined;
+  appAssert(refreshToken, UNAUTHORIZED, "Missing refresh token");
+
+  const { accessToken, newRefreshToken } =
+    await refreshUserAccessToken(refreshToken);
+
+  if (newRefreshToken) {
+    res.cookie("refreshToken", newRefreshToken, getRefreshTokenCookieOptions());
+  }
+
+  return res
+    .status(OK)
+    .cookie("accessToken", accessToken, getAccessTokenCookieOptions())
+    .json({ message: "Access token refreshed" });
 });
--- backend/src/services/auth.service.ts
+++ backend/src/services/auth.service.ts
@@ -2,10 +2,19 @@ import VerificationCodeTypes from "../constants/verificationCodeTypes.js";
 import SessionModel from "../models/session.model.js";
 import UserModel from "../models/user.model.js";
 import VerificationCodeModel from "../models/verificationCode.model.js";
-import { oneYearFromNow } from "../utils/date.js";
+import {
+  ONE_DAY_MS,
+  oneYearFromNow,
+  thirtyDaysFromNow,
+} from "../utils/date.js";
 import appAssert from "../utils/appAssert.js";
 import { CONFLICT, UNAUTHORIZED } from "../constants/http.js";
-import { refreshTokenSignOptions, signToken } from "../utils/jwt.js";
+import {
+  RefreshTokenPayload,
+  refreshTokenSignOptions,
+  signToken,
+  verifyToken,
+} from "../utils/jwt.js";
 
 export type CreateAccountParams = {
   email: string;
@@ -108,4 +117,40 @@ export const loginUser = async ({
     accessToken,
     refreshToken,
   };
+};
+
+export const refreshUserAccessToken = async (refreshToken: string) => {
+  const { payload } = verifyToken<RefreshTokenPayload>(refreshToken, {
+    secret: refreshTokenSignOptions.secret,
+  });
+  appAssert(payload, UNAUTHORIZED, "Invalid refresh token");
+
+  const session = await SessionModel.findById(payload.sessionId);
+  const now = Date.now();
+  appAssert(
+    session && session.expiresAt.getTime() > now,
+    UNAUTHORIZED,
+    "Session expired"
+  );
+
+  // refresh the session if it expires in the next 24 hours
+  const sessionNeedsRefresh = session.expiresAt.getTime() - now <= ONE_DAY_MS;
+
+  if (sessionNeedsRefresh) {
+    // You can run this on every time the access token is refreshed,
+    // instead of just on the last day before it expires
+    session.expiresAt = thirtyDaysFromNow();
+    await session.save();
+  }
+
+  const newRefreshToken = sessionNeedsRefresh
+    ? signToken({ sessionId: session._id }, refreshTokenSignOptions)
+    : undefined;
+
+  const accessToken = signToken({
+    userId: session.userId,
+    sessionId: session._id,
+  });
+
+  return { accessToken, newRefreshToken };
 };
